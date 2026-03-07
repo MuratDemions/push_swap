81b3f010a635efa1c7bcfd7ab9c45c69583df100
musipit <muratdemions@gmail.com>
2026-03-07 17:43:57 +0300

    UPDATE: header
    headerın bir kısmını daha yaptım :D

--- push_swap.h
+++ push_swap.h
@@ -6,24 +6,24 @@
 /*   By: embostan <embostan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 15:04:03 by embostan          #+#    #+#             */
-/*   Updated: 2026/03/05 15:22:04 by embostan         ###   ########.fr       */
+/*   Updated: 2026/03/06 11:57:00 by antigravity      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
 typedef struct s_node
 {
-    int             value;
-    int             index;
-    struct s_node   *next;
-    struct s_node   *prev;
-}   t_node;
+	int				value;
+	int				index;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
 
 void    sa(t_node **a);
 void    sb(t_node **b);
@@ -39,5 +39,26 @@ void    rr(t_node **a, t_node **b);
 void    rra(t_node **a);
 void    rrb(t_node **b);
 void    rrr(t_node **a, t_node **b);
+
+double	calculate_disorder(int *arr, int size);
+
+int		*parse_args(int argc, char **argv, int check_buf);
+int		ft_atoi(const char *nptr);
+int		ft_strcmp(const char *s1, const char *s2);
+
+char	*ft_itoa(int n);
+char	*writer_router(char *flag);
+
+void	ft_putstr_fd(char *s, int fd);
+void	print_move_counts(void);
+void	count_move(char *move);
+void	spt_error(void);
+void	router(char *strategy, t_node **a, t_node **b);
+void	bench_router(int argc, char **argv, t_node **a, t_node **b);
+
+void	sort_simple(t_node **a, t_node **b);
+void	sort_medium(t_node **a, t_node **b);
+void	sort_complex(t_node **a, t_node **b);
+void	sort_adaptive(t_node **a, t_node **b);
 
 #endif
